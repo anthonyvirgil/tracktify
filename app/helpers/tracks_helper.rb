8e08b3178bea49e24daaab02b4328c1967ac9a6b
anthonyvirgil <anthonyvirgil.bermejo@gmail.com>
2021-01-08 15:19:00 -0500

    Adding Trackt list feature and styling changes

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,9 +1,5 @@
 module TracksHelper
-  def display_top_tracks(tracks)
-    tracks_html = ''
-    tracks&.each_with_index do |track, index|
-      tracks_html += "<p>#{index+1}. #{track.artists.collect(&:name).join("', ")} - #{track.name}<p>"
-    end
-    return tracks_html.html_safe
+  def sanitize_track(track_name)
+    return track_name.gsub(/\(.*?\)/, '')
   end
 end
