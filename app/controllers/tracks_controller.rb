067492fc51dce5162e6190c1f5122a295dfc1036
anthonyvirgil <anthonyvirgil.bermejo@gmail.com>
2021-01-12 14:16:27 -0500

    Updated trackt list styling and buttons

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,8 +4,8 @@ class TracksController < ApplicationController
   def index
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth']) if request.env['omniauth.auth']
 
-    @top_tracks_one_month = @spotify_user&.top_tracks(limit: 10, offset: 0, time_range: 'short_term')
-    @top_tracks_six_months = @spotify_user&.top_tracks(limit: 10, offset: 0, time_range: 'medium_term')
-    @top_tracks_all_time = @spotify_user&.top_tracks(limit: 10, offset: 0, time_range: 'long_term')
+    @top_tracks_one_month = @spotify_user&.top_tracks(limit: 20, offset: 0, time_range: 'short_term')
+    @top_tracks_six_months = @spotify_user&.top_tracks(limit: 20, offset: 0, time_range: 'medium_term')
+    @top_tracks_all_time = @spotify_user&.top_tracks(limit: 20, offset: 0, time_range: 'long_term')
   end
 end
